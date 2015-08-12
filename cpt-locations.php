315a20ade12961b938ea0b79c4f6f1663db1f98d
kylestyle92 <fan_of_1978@hotmail.com>
2015-08-12 10:37:01 -0400

    Old Version of Plugin

--- cpt-locations.php
+++ cpt-locations.php
@@ -1,9 +1,9 @@
 <?php
 	/*
-	* Plugin Name: locationscct490
+	* Plugin Name: locations
 	* Description: A Custom Post Type for Locations
 	* Author: Alex - Kyle - Saad
-	* Version: 1.0
+	* Version: 2.0
 	*/
 	
 	function custom_post_type() 
@@ -49,96 +49,4 @@
 	}
 	add_action( 'init', 'custom_post_type', 0 );	// Hook into the 'init' action to generate the custom post 
 	
-// end custom post type creation ------------------------------------------------------------------------------------------------------------
-	
-	add_action( 'widgets_init', function(){register_widget( 'locations_widget' );});
-	
-	class locations_widget extends WP_Widget
-	{
-		function __construct() 
-		{
-			parent::__construct(
-				'locations_widget', // Base ID
-				'Locations Widget', // Name
-				array('description' => __( 'Displays your latest locations. Outputs the post thumbnail and title'))
-			   );
-		}
-		
-		function update($new_instance, $old_instance) 
-		{
-				$instance = $old_instance;
-				$instance['title'] = strip_tags($new_instance['title']);
-				$instance['numberOfLocations'] = strip_tags($new_instance['numberOfLocations']);
-				return $instance;
-		}
-		
-		function form($instance) 
-		{
-			if( $instance) 
-			{
-				$title = esc_attr($instance['title']);
-				$numberOfLocations = esc_attr($instance['numberOfLocations']);
-			} 
-			else 
-			{
-				$title = '';
-				$numberOfLocations = '';
-			}
-			?>
-			<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'locations_widget'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-			</p>
-			<p>
-			<label for="<?php echo $this->get_field_id('numberOfLocations'); ?>"><?php _e('Number of locations:', 'locations_widget'); ?></label>
-			<select id="<?php echo $this->get_field_id('numberOfLocations'); ?>"  name="<?php echo $this->get_field_name('numberOfLocations'); ?>">
-				<?php for($x=1;$x<=10;$x++): ?>
-				<option <?php echo $x == $numberOfLocations ? 'selected="selected"' : '';?> value="<?php echo $x;?>"><?php echo $x; ?></option>
-				<?php endfor;?>
-			</select>
-			</p>
-		<?php
-		}
-		
-		function widget($args, $instance) 
-		{
-			extract( $args );
-			$title = apply_filters('widget_title', $instance['title']);
-			$numberOfLocations = $instance['numberOfLocations'];
-			echo $before_widget;
-			if ( $title ) {
-				echo $before_title . $title . $after_title;
-			}
-			$this->getLocations($numberOfLocations);
-			echo $after_widget;
-		}	
-		
-		function getLocations($numberOfLocations) 
-		{ //html
-			global $post;
-			add_image_size( 'locations_widget_size', 85, 45, false );
-			$locations = new WP_Query();
-			$locations->query('post_type=locations&posts_per_page=' . $numberOfLocations );
-		if($locations->found_posts > 0) 
-		{
-			echo '<ul class="locations_widget">';
-				while ($locations->have_posts()) {
-					$locations->the_post();
-					$image = (has_post_thumbnail($post->ID)) ? get_the_post_thumbnail($post->ID, 'locations_widget_size') : '<div class="noThumb"></div>';
-					$listItem = '<li>' . $image;
-					$listItem .= '<a href="' . get_permalink() . '">';
-					$listItem .= get_the_title() . '</a>';
-					$listItem .= '<span>Added ' . get_the_date() . '</span></li>';
-					echo $listItem;
-				}
-			echo '</ul>';
-			wp_reset_postdata();
-		}
-		else
-		{
-			echo '<p>No listing found</p>';
-		}
-		
-		}
-	}
 ?>
